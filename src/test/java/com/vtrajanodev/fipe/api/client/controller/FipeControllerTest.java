3de77f1ab17a0829905fcce9b449a26336f7c316
Victor Trajano <victor.silva@koper.com.br>
2025-12-02 20:08:08 -0300

    fix tests

--- src/test/java/com/vtrajanodev/fipe/api/client/controller/FipeControllerTest.java
+++ src/test/java/com/vtrajanodev/fipe/api/client/controller/FipeControllerTest.java
@@ -12,7 +12,6 @@ import org.mockito.junit.jupiter.MockitoExtension;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 
-import java.math.BigDecimal;
 import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
@@ -30,10 +29,7 @@ class FipeControllerTest {
 
   @Test
   void testListBrands() {
-    List<FipeItemResponse> mockBrands = List.of(
-            new FipeItemResponse("1", "Ford"),
-            new FipeItemResponse("2", "Chevrolet")
-    );
+    List<FipeItemResponse> mockBrands = List.of(new FipeItemResponse("1", "Ford"), new FipeItemResponse("2", "Chevrolet"));
 
     when(service.listBrands("cars")).thenReturn(mockBrands);
 
@@ -42,7 +38,7 @@ class FipeControllerTest {
     assertNotNull(response);
     assertEquals(HttpStatus.OK, response.getStatusCode());
 
-    assertNotNull(response.getBody()); // <-- remove o warning
+    assertNotNull(response.getBody());
     assertEquals(2, response.getBody().size());
     assertEquals("Ford", response.getBody().get(0).getName());
 
@@ -51,10 +47,7 @@ class FipeControllerTest {
 
   @Test
   void testListModels() {
-    List<FipeItemResponse> mockModels = List.of(
-            new FipeItemResponse("10", "Fiesta"),
-            new FipeItemResponse("20", "Focus")
-    );
+    List<FipeItemResponse> mockModels = List.of(new FipeItemResponse("10", "Fiesta"), new FipeItemResponse("20", "Focus"));
 
     when(service.listModels("cars", "1")).thenReturn(mockModels);
 
@@ -72,23 +65,11 @@ class FipeControllerTest {
 
   @Test
   void testGetPrice() {
-    FipePriceResponse mockPrice = new FipePriceResponse(
-            "Ford",        // brand
-            "001267-0",    // codeFipe
-            "Gasolina",    // fuel
-            "G",           // fuelAcronym
-            "Fiesta",      // model
-            2019,          // modelYear (Integer)
-            "45000",       // price (String!)
-            "setembro de 2024", // referenceMonth
-            "cars"         // vehicleType
-    );
+    FipePriceResponse mockPrice = new FipePriceResponse("Ford", "001267-0", "Gasolina", "G", "Fiesta", 2019, "45000", "setembro de 2024", "cars");
 
-    when(service.getPriceByYear("cars", "1", "10", "2019"))
-            .thenReturn(mockPrice);
+    when(service.getPriceByYear("cars", "1", "10", "2019")).thenReturn(mockPrice);
 
-    ResponseEntity<FipePriceResponse> response =
-            controller.getPrice("cars", "1", "10", "2019");
+    ResponseEntity<FipePriceResponse> response = controller.getPrice("cars", "1", "10", "2019");
 
     assertNotNull(response);
     assertEquals(HttpStatus.OK, response.getStatusCode());
@@ -101,36 +82,16 @@ class FipeControllerTest {
     assertEquals(2019, body.getModelYear());
     assertEquals("45000", body.getPrice());
 
-    verify(service, times(1))
-            .getPriceByYear("cars", "1", "10", "2019");
+    verify(service, times(1)).getPriceByYear("cars", "1", "10", "2019");
   }
 
   @Test
   void testGetPriceHistory() {
-    List<VehicleFipeInformationResponse> mockHistory = List.of(
-            new VehicleFipeInformationResponse(
-                    "2018",     // year
-                    "40000",    // price
-                    null,       // diff
-                    null,       // diffPercentage
-                    null,       // previousYear
-                    null        // previousPrice
-            ),
-            new VehicleFipeInformationResponse(
-                    "2019",
-                    "45000",
-                    null,
-                    null,
-                    null,
-                    null
-            )
-    );
+    List<VehicleFipeInformationResponse> mockHistory = List.of(new VehicleFipeInformationResponse("2018", "40000", null, null, null, null), new VehicleFipeInformationResponse("2019", "45000", null, null, null, null));
 
-    when(service.getPriceHistoryDetailed("cars", "1", "10"))
-            .thenReturn(mockHistory);
+    when(service.getPriceHistoryDetailed("cars", "1", "10")).thenReturn(mockHistory);
 
-    ResponseEntity<List<VehicleFipeInformationResponse>> response =
-            controller.getPriceHistory("cars", "1", "10");
+    ResponseEntity<List<VehicleFipeInformationResponse>> response = controller.getPriceHistory("cars", "1", "10");
 
     assertNotNull(response);
     assertEquals(HttpStatus.OK, response.getStatusCode());
@@ -144,8 +105,7 @@ class FipeControllerTest {
     assertEquals("2019", response.getBody().get(1).getYear());
     assertEquals("45000", response.getBody().get(1).getPrice());
 
-    verify(service, times(1))
-            .getPriceHistoryDetailed("cars", "1", "10");
+    verify(service, times(1)).getPriceHistoryDetailed("cars", "1", "10");
   }
 }
 
