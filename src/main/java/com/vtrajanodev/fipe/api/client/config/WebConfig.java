7f5e6752d8be4dbe55852c39f48474d609a1a90d
Victor Trajano <victor.silva@koper.com.br>
2025-12-03 09:24:26 -0300

    fix: cors

--- src/main/java/com/vtrajanodev/fipe/api/client/config/WebConfig.java
+++ src/main/java/com/vtrajanodev/fipe/api/client/config/WebConfig.java
@@ -14,7 +14,10 @@ public class WebConfig {
       @Override
       public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOrigins("http://localhost:4200")
+                .allowedOrigins(
+                        "http://localhost:4200",
+                        "https://fipe-search-frontend.vercel.app"
+                )
                 .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
                 .allowCredentials(true);
       }
